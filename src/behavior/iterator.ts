ff50e255888b5f05c831a906962ee3dd716ce706
luis-vanin <vanin@copybase.com.br>
2023-06-08 19:38:43 -0300

    fix: adjusting and reimplemeting iterator

--- src/behavior/iterator.ts
+++ src/behavior/iterator.ts
@@ -6,6 +6,10 @@ export interface IteratorProtocol<T> {
     collect(): Array<T> | null;
 }
 
+interface IterableProtocol<T> {
+    getIterator(): IteratorProtocol<T>;
+}
+
 export class LinkedNode<T> {
     next: LinkedNode<T> | null;
     value: T;
@@ -16,28 +20,18 @@ export class LinkedNode<T> {
     }
 }
 
-export class LinkedList<T> implements IteratorProtocol<LinkedNode<T>> {
+export class LinkedList<T> implements IterableProtocol<T> {
     public head: LinkedNode<T> | null;
     public tail: LinkedNode<T> | null;
-    private currentNode: LinkedNode<T> | null;
     public length: number;
 
     constructor() {
         this.head = null;
         this.tail = null;
         this.length = 0;
-        this.currentNode = null;
     }
 
-    navigate(callback: (node: LinkedNode<T>) => void) {
-        let node = this.head;
-        while (node !== null) {
-            callback(node);
-            node = node.next;
-        }
-    }
-
-    add(value: T) {
+    push(value: T) {
         let Linkednode = new LinkedNode<T>(value);
         if (this.head === null) {
             this.head = Linkednode;
@@ -50,31 +44,129 @@ export class LinkedList<T> implements IteratorProtocol<LinkedNode<T>> {
         this.length++;
     }
 
-    rewind(): void {
+    navigate(callback: (node: LinkedNode<T>) => void) {
+        let node = this.head;
+        while (node !== null) {
+            callback(node);
+            node = node.next;
+        }
+    }
+
+    pop(): LinkedNode<T> | null {
+        if (this.head === null) return null;
+        let node = this.head;
+        let prev = null;
+        while (node.next !== null) {
+            prev = node;
+            node = node.next;
+        }
+        if (prev) prev.next = null;
+        this.tail = prev;
+        this.length--;
+        return node;
+    }
+
+    getIterator(): IteratorProtocol<T> {
+        return new LinkedListIterator(this);
+    }
+}
+
+export class LinkedListIterator<T> implements IteratorProtocol<T> {
+    private list: LinkedList<T>;
+    private currentNode: LinkedNode<T> | null;
+
+    constructor(list: LinkedList<T>) {
+        this.list = list;
         this.currentNode = null;
     }
 
-    next(): LinkedNode<T> | undefined {
-        if (!this.head) throw new Error("List is empty");
+    next(): T | undefined {
+        if (!this.list.head) throw new Error("List is empty");
         if (!this.currentNode) {
-            this.currentNode = this.head;
-            return this.currentNode;
+            this.currentNode = this.list.head;
+            return this.currentNode.value;
         }
         if (this.currentNode.next === null) {
             this.rewind();
             return undefined;
         }
         this.currentNode = this.currentNode.next;
-        return this.currentNode as LinkedNode<T>;
+        return this.currentNode.value;
     }
 
-    collect() {
-        if (!this.head && !this.tail) return null;
-        let list: Array<LinkedNode<T>> = [];
-        this.navigate((node) => list.push(node));
-        return list;
+    rewind(): void {
+        this.currentNode = null;
+    }
+
+    collect(): Array<T> | null {
+        if (!this.list.head && !this.list.tail) return [];
+        let result: Array<T> = [];
+        this.list.navigate((node) => {
+            result.push(node.value);
+        });
+        return result;
     }
 }
+
+// export class LinkedList<T> implements IteratorProtocol<LinkedNode<T>> {
+//     public head: LinkedNode<T> | null;
+//     public tail: LinkedNode<T> | null;
+//     private currentNode: LinkedNode<T> | null;
+//     public length: number;
+
+//     constructor() {
+//         this.head = null;
+//         this.tail = null;
+//         this.length = 0;
+//         this.currentNode = null;
+//     }
+
+//     navigate(callback: (node: LinkedNode<T>) => void) {
+//         let node = this.head;
+//         while (node !== null) {
+//             callback(node);
+//             node = node.next;
+//         }
+//     }
+
+//     add(value: T) {
+//         let Linkednode = new LinkedNode<T>(value);
+//         if (this.head === null) {
+//             this.head = Linkednode;
+//             this.tail = Linkednode;
+//         } else {
+//             if (!this.tail) throw new Error("Tail is null");
+//             this.tail.next = Linkednode;
+//             this.tail = Linkednode;
+//         }
+//         this.length++;
+//     }
+
+//     rewind(): void {
+//         this.currentNode = null;
+//     }
+
+//     next(): LinkedNode<T> | undefined {
+//         if (!this.head) throw new Error("List is empty");
+//         if (!this.currentNode) {
+//             this.currentNode = this.head;
+//             return this.currentNode;
+//         }
+//         if (this.currentNode.next === null) {
+//             this.rewind();
+//             return undefined;
+//         }
+//         this.currentNode = this.currentNode.next;
+//         return this.currentNode as LinkedNode<T>;
+//     }
+
+//     collect() {
+//         if (!this.head && !this.tail) return null;
+//         let list: Array<LinkedNode<T>> = [];
+//         this.navigate((node) => list.push(node));
+//         return list;
+//     }
+// }
 
 // interface IteratorProtocol<T> {
 //     next(): T | undefined;
--- tests/behavior/iterator.spec.ts
+++ tests/behavior/iterator.spec.ts
@@ -1,4 +1,4 @@
-import { LinkedList } from "../../src/behavior";
+import { LinkedList, LinkedListIterator } from "../../src/behavior";
 
 const generateNumber = () => {
     return Math.floor(Math.random() * 100);
@@ -14,54 +14,65 @@ describe("Behavioral Patterns --Iterator--", () => {
             ];
             const linked = new LinkedList<number>();
 
-            linked.add(rndList[0]);
-            linked.add(rndList[1]);
-            linked.add(rndList[2]);
+            linked.push(rndList[0]);
+            linked.push(rndList[1]);
+            linked.push(rndList[2]);
 
             expect(linked.head?.value).toBe(rndList[0]);
             expect(linked.tail?.value).toBe(rndList[2]);
 
             expect(linked.length).toBe(3);
 
-            const simpledList = linked.collect()?.map((node, idx) => {
-                expect(node.value).toBe(rndList[idx]);
-                return node.value;
-            });
+            const simpledList = linked.getIterator().collect();
 
             expect(simpledList).toStrictEqual(rndList);
+
+            const node = linked.pop();
+            expect(node?.value).toBe(rndList.at(-1));
+
+            expect(linked.length).toBe(2);
+
+            expect(linked.tail?.value).toBe(rndList[1]);
         });
 
         it("Testing linked list iterator", () => {
+            const linkedlist = new LinkedList<number>();
             const rndList = [
                 generateNumber(),
                 generateNumber(),
                 generateNumber(),
             ];
-            const linked = new LinkedList<number>();
+            let iterator = linkedlist.getIterator();
 
-            linked.add(rndList[0]);
-            linked.add(rndList[1]);
-            linked.add(rndList[2]);
+            expect(iterator).toBeInstanceOf(LinkedListIterator);
+            expect(iterator.collect()).toStrictEqual([]);
 
-            let value = linked.next();
-            expect(value?.value).toBe(rndList[0]);
-            value = linked.next();
-            expect(value?.value).toBe(rndList[1]);
-            value = linked.next();
-            expect(value?.value).toBe(rndList[2]);
-            value = linked.next();
-            expect(value).toBeUndefined();
+            linkedlist.push(rndList[0]);
+            linkedlist.push(rndList[1]);
+            linkedlist.push(rndList[2]);
 
-            linked.rewind();
+            expect(iterator.collect()).toStrictEqual(rndList);
 
-            value = linked.next();
-            expect(value?.value).toBe(rndList[0]);
+            iterator = linkedlist.getIterator();
+            expect(iterator.next()).toBe(rndList[0]);
+            expect(iterator.next()).toBe(rndList[1]);
+            expect(iterator.next()).toBe(rndList[2]);
+            expect(iterator.next()).toBeUndefined();
+
+            iterator.rewind();
+
+            for (let i = 0; i < rndList.length; i++) {
+                expect(iterator.next()).toBe(rndList[i]);
+            }
+            expect(iterator.next()).toBeUndefined();
         });
 
         it("Testing linked list iterator with empty list", () => {
             const linked = new LinkedList<number>();
-            expect(linked.collect()).toBeNull();
-            expect(() => linked.next()).toThrowError("List is empty");
+            expect(linked.getIterator().collect()).toStrictEqual([]);
+            expect(() => linked.getIterator().next()).toThrowError(
+                "List is empty"
+            );
         });
     });
 });
